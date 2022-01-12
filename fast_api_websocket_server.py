aa8dda8b6d4e387c7d96e696a6bcc7469696bf8d
syedafrith <73065970+syedafrith@users.noreply.github.com>
2022-01-12 20:27:06 +0530

    Add files via upload
    first commit

---
+++ fast_api_websocket_server.py
@@ -0,0 +1,105 @@
+import logging
+import json
+from collections import defaultdict
+
+import uvicorn as uvicorn
+from fastapi import FastAPI, WebSocket, Request, Depends, BackgroundTasks
+from starlette.websockets import WebSocketDisconnect
+from starlette.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # can alter with time
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+class Notifier:
+    """
+        Manages chat room sessions and members along with message routing
+    """
+
+    def __init__(self):
+        self.connections: dict = defaultdict(dict)
+        self.generator = self.get_notification_generator()
+
+    async def get_notification_generator(self):
+        while True:
+            message = yield
+            msg = message["message"]
+            room_name = message["room_name"]
+            await self._notify(msg, room_name)
+
+    def get_members(self, room_name):
+        try:
+            return self.connections[room_name]
+        except Exception:
+            return None
+
+    async def push(self, msg: str, room_name: str = None):
+        message_body = {"message": msg, "room_name": room_name}
+        await self.generator.asend(message_body)
+
+    async def connect(self, websocket: WebSocket, room_name: str):
+        await websocket.accept()
+        if self.connections[room_name] == {} or len(self.connections[room_name]) == 0:
+            self.connections[room_name] = []
+        self.connections[room_name].append(websocket)
+        # print(f"CONNECTIONS : {self.connections[room_name]}")
+
+    def remove(self, websocket: WebSocket, room_name: str):
+        self.connections[room_name].remove(websocket)
+        # print(
+        #     f"CONNECTION REMOVED\nREMAINING CONNECTIONS : {self.connections[room_name]}"
+        # )
+
+    async def _notify(self, message: str, room_name: str):
+        living_connections = []
+        while len(self.connections[room_name]) > 0:
+
+            websocket = self.connections[room_name].pop()
+            await websocket.send_text(str(message))
+            living_connections.append(websocket)
+        self.connections[room_name] = living_connections
+
+
+notifier = Notifier()
+
+
+
+@app.websocket("/ws/{room_name}/")
+async def websocket_endpoint(
+    websocket: WebSocket, room_name, background_tasks: BackgroundTasks
+):
+    await notifier.connect(websocket, room_name)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            try:
+                d = json.loads(data)
+                d["room_name"] = room_name
+            except:
+                pass
+
+            room_members = (
+                notifier.get_members(room_name)
+                if notifier.get_members(room_name) is not None
+                else []
+            )
+            if websocket not in room_members:
+                print("SENDER NOT IN ROOM MEMBERS: RECONNECTING")
+                await notifier.connect(websocket, room_name)
+
+            await notifier._notify(f"{data}", room_name)
+    except WebSocketDisconnect:
+            notifier.remove(websocket, room_name)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
